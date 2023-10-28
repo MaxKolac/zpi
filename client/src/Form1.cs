2c0cd750a95756f2121fc2996435ad7f72232bec
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-10-28 21:34:19 +0200

    Added initial client files

---
+++ client/src/Form1.cs
@@ -0,0 +1,10 @@
+namespace ZPIClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ client/test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ZPIClientTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
