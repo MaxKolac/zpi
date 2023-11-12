9890062bcd6b46827294eb168c592e4a979949b0
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-11-12 22:58:52 +0100

    Made Program.cs fully static

--- server/src/Program.cs
+++ server/src/Program.cs
@@ -1,30 +1,49 @@
-﻿using ZPIServer.API;
+﻿using System.Diagnostics;
+using ZPIServer.API;
+using ZPIServer.Commands;
+using ZPIServer.EventArgs;
 
 namespace ZPIServer
 {
-    public class Program
+    public static class Program
     {
         static TcpHandler? tcpHandler;
         static SignalTranslator? signalTranslator;
 
-        static void Main(string[] args)
+
+        public static int Main(string[] args)
         {
             StartServer();
             while (true)
             {
                 //server lifetime loop
+                Console.Write(">> ");
+                string? consoleInput = Console.ReadLine();
+                HandleCommand(consoleInput);
             }
+            StopServer();
+            return 0;
         }
 
-        static void StartServer()
+        private static void StartServer()
         {
+            var stopwatch = Stopwatch.StartNew();
             tcpHandler = new TcpHandler(Settings.ServerAddress, Settings.TcpListeningPort);
             tcpHandler.BeginListening();
             signalTranslator = new SignalTranslator();
             signalTranslator.BeginTranslating();
+            stopwatch.Stop();
+
+            OnCommandExecuted += OnCommandExecutionInvoke;
+
+            Console.WriteLine($"Done! Server took {stopwatch.Elapsed.TotalMilliseconds} milliseconds to start up.");
         }
 
         static void StopServer()
+            command.Execute();
+        }
+
+        private static void StopServer()
         {
             signalTranslator?.StopTranslating();
             tcpHandler?.StopListening();
