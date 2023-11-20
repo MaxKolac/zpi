1936728b9fd79f18fe054775b2a5c703d38e843f
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-11-20 18:08:30 +0100

    Added Json converters and CameraDataMessage class

---
+++ communicationModels/JsonConverters/IPAddressConverter.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace ZPICommunicationModels.JsonConverters;
+
+/// <summary>
+/// <see cref="IPAddress"/> nie jest klasą przyjazną dla serializowania - wymaga osobnego konwertera.<br/>
+/// Zarejestruj konwerter przed serializowaniem/deserializowaniem w taki sposób:
+/// <code>
+/// var settings = new JsonSerializerSettings();
+/// settings.Converters.Add(new <see cref="IPAddressConverter"/>());
+/// string json = JsonConvert.SerializeObject(object, settings);
+/// </code>
+/// Źródło: <see href="https://pingfu.net/how-to-serialise-ipaddress-ipendpoint"/>
+/// </summary>
+public class IPAddressConverter : JsonConverter
+{
+    public override bool CanConvert(Type objectType)
+    {
+        return (objectType == typeof(IPAddress));
+    }
+
+    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+    {
+        var address = value.ToString();
+        JToken.FromObject(address).WriteTo(writer);
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+    {
+        var address = JToken.Load(reader).ToString();
+        return IPAddress.Parse(address);
+    }
+}
---
+++ communicationModels/JsonConverters/IPEndPointConverter.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace ZPICommunicationModels.JsonConverters;
+
+/// <summary>
+/// <see cref="IPEndPoint"/> nie jest klasą przyjazną dla serializowania - wymaga osobnego konwertera.<br/>
+/// Zarejestruj konwerter przed serializowaniem/deserializowaniem w taki sposób:
+/// <code>
+/// var settings = new JsonSerializerSettings();
+/// settings.Converters.Add(new <see cref="IPEndPointConverter"/>());
+/// string json = JsonConvert.SerializeObject(object, settings);
+/// </code>
+/// Źródło: <see href="https://gist.github.com/marcbarry/2e7a64fed2ae539cf415"/>
+/// </summary>
+public class IPEndPointConverter : JsonConverter
+{
+    public override bool CanConvert(Type objectType)
+    {
+        return objectType == typeof(IPEndPoint);
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+    {
+        return JToken.Load(reader).ToString().ToIPEndPoint();
+    }
+
+    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+    {
+        if (value is IPEndPoint ipEndPoint)
+        {
+            if (ipEndPoint.Address != null || ipEndPoint.Port != 0)
+            {
+                JToken.FromObject(string.Format("{0}:{1}", ipEndPoint.Address, ipEndPoint.Port)).WriteTo(writer);
+                return;
+            }
+        }
+        writer.WriteNull();
+    }
+}
+
+public static class IPAddressExtensions
+{
+    public static IPEndPoint? ToIPEndPoint(this string ipEndPoint)
+    {
+        if (string.IsNullOrWhiteSpace(ipEndPoint))
+        {
+            return null;
+        }
+        var components = ipEndPoint.Split(':');
+        return new IPEndPoint(IPAddress.Parse(components[0]), Convert.ToInt32(components[1]));
+    }
+}
