d01bccb18c84bf6c716d6de73e6a895404241a02
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-11-21 12:14:37 +0100

    Added a check to ensure all Python dependencies are present

--- server/src/API/CameraLibraries/PythonCameraSimulatorAPI.cs
+++ server/src/API/CameraLibraries/PythonCameraSimulatorAPI.cs
@@ -6,6 +6,7 @@ namespace ZPIServer.API.CameraLibraries;
 
 public class PythonCameraSimulatorAPI : ICamera
 {
+    private const string PythonPrefix = "Python";
     private readonly Logger? _logger;
     private CameraDataMessage? _message;
 
@@ -71,5 +72,60 @@ public class PythonCameraSimulatorAPI : ICamera
             returnCode = -1;
         }
         return returnCode;
+    }
+
+    /// <summary>
+    /// Sprawdza obecność tych zależności w danych wersjach:
+    /// <code>
+    /// Pillow 10.0.0
+    /// numpy 1.25.0
+    /// opencv-contrib-python 4.8.1.78
+    /// python-dotenv 1.0.0
+    /// </code>
+    /// </summary>
+    private void CheckPythonPackagesInstallation()
+    {
+        var dependencies = new string[]
+        {
+            "Pillow==10.0.0",
+            "numpy==1.25.0",
+            "opencv-contrib-python==4.8.1.78",
+            "python-dotenv==1.0.0"
+        };
+
+        var startInfos = new ProcessStartInfo[dependencies.Length];
+        for (int i = 0; i < startInfos.Length; i++)
+        {
+            startInfos[i] = new ProcessStartInfo()
+            {
+                FileName = @"python.exe",
+                Arguments = "-m pip install " + dependencies[i],
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            };
+        }
+        
+        foreach (var startInfo in startInfos) 
+        {
+            using var process = Process.Start(startInfo);
+            using var standardReader = process!.StandardOutput;
+            using var errorReader = process!.StandardError;
+            Task standardOutput = Task.Run(() =>
+            {
+                while (!standardReader.EndOfStream)
+                {
+                    _logger?.WriteLine(standardReader.ReadLine(), PythonPrefix);
+                }
+            });
+            Task errorOutput = Task.Run(() =>
+            {
+                while (!errorReader.EndOfStream)
+                {
+                    _logger?.WriteLine(errorReader.ReadLine(), PythonPrefix, Logger.MessageType.Error);
+                }
+            });
+            Task.WaitAll(standardOutput, errorOutput);
+        }
     }
 }
