1c0f84076b2f4e6e41c755a8941901c709ec7398
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-11-21 15:23:50 +0100

    Added ability to send JSON messages in loop

---
+++ camera-simulator/src/CameraSimulator.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net.Sockets;
+using System.Text;
+using ZPICommunicationModels.Messages;
+using ZPICommunicationModels.Models;
+
+namespace ZPICameraSimulator;
+
+internal class CameraSimulator
+{
+    public static void SendJson(CameraDataMessage message)
+    {
+        if (!OperatingSystem.IsWindows())
+            return;
+
+        using var server = new TcpClient();
+        server.Connect("127.0.0.1", 25566);
+
+        string json = JsonConvert.SerializeObject(message);
+        using (var stream = server.GetStream())
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(json);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+        server.Close();
+    }
+}
--- camera-simulator/src/Program.cs
+++ camera-simulator/src/Program.cs
@@ -1,9 +1,52 @@
-﻿namespace ZPICameraSimulator;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using ZPICommunicationModels.Messages;
+using ZPICommunicationModels.Models;
 
-internal class Program
+namespace ZPICameraSimulator;
+
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if (!OperatingSystem.IsWindows())
+            return;
+
+        while (true)
+        {
+            CameraDataMessage message = new()
+            {
+                LargestTemperature = -69.6969m,
+                Image = HostDevice.ToByteArray(Image.FromFile("test.png"), ImageFormat.Png) ?? Array.Empty<byte>(),
+                Status = HostDevice.DeviceStatus.OK
+            };
+
+            Console.WriteLine("Dane, które symulator spróbuje wysłać na serwer:");
+            Console.WriteLine($"Największa temperatura: {message.LargestTemperature}");
+            Console.WriteLine($"Obraz: {message.Image.Length} bajtów");
+            Console.WriteLine($"Status: {message.Status}");
+            Console.WriteLine();
+            Console.WriteLine("1. Wyślij jako JSON.");
+            var key = Console.ReadKey();
+
+            try
+            {
+                switch (key.Key)
+                {
+                    case ConsoleKey.D1:
+                        CameraSimulator.SendJson(message);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.ReadKey();
+            Console.WriteLine("------");
+        }
     }
 }
