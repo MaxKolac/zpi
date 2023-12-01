3f5e9d61ebb4d177cf899c8289f30ad7bc58bd7d
Filip <blazerwarrior@gmail.com>
2023-12-01 19:31:50 +0100

    Przeniesienie stałych do pliku .py zamiast pliku .env

---
+++ thermalImageParser/constants.template.py
@@ -0,0 +1,5 @@
+ENDPOINT_URL = None #Your endpoint url#
+API_KEY = None #Api key for application to verify itself#
+TEMP_MIN = None #Minimal temperature for regular picture files#
+TEMP_MAX = None #Maximal temperature for regular picture files#
+RADIUS = None #Radius to check around hottest pixel to calculate mean#
