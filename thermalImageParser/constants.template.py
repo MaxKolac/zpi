7bba689a02809c61facbec6593db03c24f67a353
Filip <blazerwarrior@gmail.com>
2023-12-01 19:33:04 +0100

    Update do pliku ze stałymi

--- thermalImageParser/constants.template.py
+++ thermalImageParser/constants.template.py
@@ -1,3 +1,9 @@
+"""
+    Copy this file and rename the copy to constants.py
+
+    fill out all necessary informations before starting app
+"""
+
 ENDPOINT_URL = None #Your endpoint url#
 API_KEY = None #Api key for application to verify itself#
 TEMP_MIN = None #Minimal temperature for regular picture files#
